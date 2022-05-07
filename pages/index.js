1661e32e62b3f42000ff19610d9af21ab50ff7bd
Asif Istiaque <asifistiaque.ai@gmail.com>
2022-05-07 17:31:27 +0600

    ss

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { Router, useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Page from '../components/nav/Page/Page';
+import useAuth from '../hooks/useAuth';
 
 export default function Home() {
+	const router = useRouter();
+	const auth = useAuth();
+	useEffect(() => {
+		if (!auth.isLoggedIn) {
+			router.push('/login');
+		}
+	}, [auth.loading]);
+	if (auth.loading) return null;
 	return (
 		<Page selected='Dashboard'>
 			<h4>Dashboard</h4>
